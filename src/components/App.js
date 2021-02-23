b8f45c65d2a3eccf4cb86652838a100e434a28eb
Daria Viter <viter.daria@gmail.com>
2021-02-23 16:16:21 +0000

    запустила build

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-import React from "react";
-import data from "../data/data.js";
+import React from 'react';
+import data from '../data/data.js';
 
-import Profile from "./profile/Profile.js";
-import Statistics from "./statistics/Statistics.js";
-import FriendList from "./friendList/FriendList.js";
-import TransactionHistory from "./transactions/Transactions.js";
-
+import Profile from './profile/Profile.js';
+import Statistics from './statistics/Statistics.js';
+import FriendList from './friendList/FriendList.js';
+import TransactionHistory from './transactions/Transactions.js';
 
 const App = () => {
-    // return (<Profile        
-    //     {...data.user}
-    // />);
+  return (
+    <>
+      <Profile {...data.user} />
 
-    // return (<Statistics title="UPLOAD STATS" stats={data.stats} />)
+      <Statistics title="UPLOAD STATS" stats={data.stats} />
 
-    return (<FriendList friends={data.friends}/>)
+      <FriendList friends={data.friends} />
 
-    // return <TransactionHistory items={data.transactions} />
-}
+      <TransactionHistory items={data.transactions} />
+    </>
+  );
+};
 
 export default App;
